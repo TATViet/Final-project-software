5e4ee8f2969962c030a7f62e0f522aa4d1363dee
TATViet <153576362+TATViet@users.noreply.github.com>
2025-01-09 18:12:33 +0700

    Update app.js firewall
    creat helmet
    and 
    rateLimit

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const path = require('path');
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const port = process.env.PORT || 3000;
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 phút
+    max: 100, // giới hạn mỗi IP tối đa 100 yêu cầu trong khoảng thời gian 15 phút
+    message: 'Bạn đã gửi quá nhiều yêu cầu. Vui lòng thử lại sau vài phút.'
+});
+
+
+function checkAuthentication(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next(); // Người dùng đã đăng nhập, cho phép truy cập
+    }
+    res.redirect('/login'); // Chuyển hướng đến trang đăng nhập
+}
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://TracViet:Vietk12+@atlascluster.3tjpvpi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,6 +36,8 @@ app.use(helmet.hsts({
 })); //HSTS giúp bảo vệ người dùng khỏi các tấn công man-in-the-middle bằng cách đảm bảo rằng các yêu cầu luôn được gửi qua HTTPS.
 app.use(helmet.noSniff());// ngăn chặn trình duyệt xác định kiểu MIME  bảo vệ ứng dụng khỏi tấn công XSS
 
+// Sử dụng middleware cho tất cả các yêu cầu
+app.use(limiter);
 
 // Middleware setup
 app.set("view engine", "ejs");
